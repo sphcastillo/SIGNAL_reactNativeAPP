e97e9c8fba7633050fad1802873072df2d1e4482
Sophia Castillo <Sphcastillo@gmail.com>
2022-10-20 22:04:05 -0700

    add Firebase Config on firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDT_4iP8iScIgU0LblM_Ai6-GZTe65CATE",
+    authDomain: "signal-clone-33795.firebaseapp.com",
+    projectId: "signal-clone-33795",
+    storageBucket: "signal-clone-33795.appspot.com",
+    messagingSenderId: "945994309662",
+    appId: "1:945994309662:web:9e87a4647358336f0cce0f"
+};
